37c2548fd9f116504af9690522ca3253aa7f68d3
Beristain20 <abcortes.03@gmail.com>
2016-04-28 15:02:11 -0500

    incluye el texto descriptivo en tooltip

--- src/utilerias/Constantes.java
+++ src/utilerias/Constantes.java
@@ -19,9 +19,21 @@ public class Constantes {
 	public static final String GAUSSIAN_BLUR = "GAUSSIAN_BLUR";
 	public static final List<String> FILTROS = Arrays.asList(ESCALA_GRISES, BLUR, GAUSSIAN_BLUR);
 	// CONSTANTES FILSTROS TOOLTIPS
-	public static final String TOOLTIP_ESCALA_GRISES = "ESTO ES UN TOOLTIP PARA ESCALA_GRISES";
-	public static final String TOOLTIP_BLUR = "ESTO ES UN TOOLTIP PARA BLUR";
-	public static final String TOOLTIP_GAUSSIAN_BLUR = " ESTO ES UN TOOLTIP PARA GAUSSIAN_BLUR";
+	public static final String TOOLTIP_ESCALA_GRISES = "Convierte imagen original a escala de grises para poder aplicar un filtro.";
+	public static final String TOOLTIP_BLUR = "Suaviza (desenfoca) la imagen original de tal forma que este se visualiza borrosa.\\ "
+			+ "src -recibe la  imagen de entrada.\\"
+			+ "dst - recibe la imagen de salida del mismo tamaño y tipo que el de entrada.\\"
+			+ "Tamaño - .Tamaño del núcleo a utilizar (ancho de píxeles y alto de píxeles).\\"
+			+ "ancla - Píxel central predeterminado en el punto (-1, -1) significa que  está en el\\"
+			+ "centro situado con respecto a los píxeles vecinos.";
+	public static final String TOOLTIP_GAUSSIAN_BLUR = " Suaviza una imagen para mejorar la calidad de visualización eliminando\\"
+			+ "los granos conocidos como ruido que hacen borrosa una imagen.\\"
+			+ "src - recibe la imagen de entrada.//"
+			+ "dst - recibe la imagen de salida del mismo tamaño y tipo que el de entrada.\\"
+			+ "Tamaño -. Tamaño del núcleo a utilizar (ancho de píxeles y alto de píxeles).\\"
+			+ "puede ser diferente, pero ambos deben ser positivos e impar. O bien, pueden ser cero\\"
+			+ "Sigmax - desviación estándar del núcleo de Gauss en la dirección X.\\"
+			+ "Sigmay - Gauss desviación estándar del núcleo en la dirección Y.";
 	// CONSTANTES FILTROS FIN
 
 	// CONSTANTES DETECCION DE OBJETOS INICIO
